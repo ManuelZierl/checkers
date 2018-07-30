d2d03c57d3e02e7af1000363198626c8d4f9cf06
ru76sin <manuel.zierl@campus.lmu.de>
2018-07-30 10:10:07 +0200

    train a model

--- Play.py
+++ Play.py
@@ -5,7 +5,9 @@ from Human_Player import Human_Player
 from KI_Player import KI_Player
 
 player_1 = Human_Player(12, 0)
-player_2 = KI_Player(12, 1, model=load_model("models/winnerm.h5"))
+player_2 = KI_Player(12, 1, model=load_model("models/1000_20.h5"))
 
 game = Game(player_1=player_1, player_2=player_2)
 print(game.play(show=True))
+player_2.train(game.history_board, game.history_reward_WHITE)
+player_1.save("1000_20_besser.h5")
